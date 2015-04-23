c6e46fcb67e7d1f8f9836bfd4675f056c3b56ab8
tim <tjhickey@brandeis.edu>
2015-04-23 14:52:06 -0400

    fixed typo

--- client/answersByStudent.js
+++ client/answersByStudent.js
@@ -1,9 +1,4 @@
-Template.answersByStudent.helpers({
-	students:function()
-	
-	
-	{return Answers.find({}).distinct({'student', true})}
-	})
+
 
 Template.studentoption.helpers({
 	email:function(){return this.emails[0].address;}
