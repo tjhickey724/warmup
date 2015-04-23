8f2387955e63cf3f8256040f70f1197b1e446ff6
Danielle Pike <DAP@Danielles-MacBook-Pro.local>
2015-04-23 15:36:18 -0400

    fixed answer tables

--- client/answersByStudent.js
+++ client/answersByStudent.js
@@ -1,4 +1,6 @@
-
+Template.answersByStudent.helpers({
+answers: function(){return Answers.find({}, {sort:{student:1, date:-1}})}
+})
 
 Template.studentoption.helpers({
 	email:function(){return this.emails[0].address;}
