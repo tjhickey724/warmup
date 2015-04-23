2b23202d868e009bafaa929dfc21fd80ea32e6db
Danielle Pike <DAP@Danielles-MacBook-Pro.local>
2015-04-23 14:53:25 -0400

    fixed typo

--- client/answersByStudent.js
+++ client/answersByStudent.js
@@ -1,9 +1,3 @@
-Template.answersByStudent.helpers({
-	students:function()
-	
-	
-	{return Answers.find({}).distinct({'student', true})}
-	})
 
 Template.studentoption.helpers({
 	email:function(){return this.emails[0].address;}
